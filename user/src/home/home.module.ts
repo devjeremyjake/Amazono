e48e983ffd98f884ff0416247536f45986237bb9
devjeremyjake <48024326+devjeremyjake@users.noreply.github.com>
2019-05-20 10:45:52 +0100

    Add files via upload

---
+++ user/src/home/home.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { NouisliderModule } from 'ng2-nouislider';
+import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
+
+import { HomeComponent } from './home.component';
+
+import { ComponentsModule } from '../components/components.module';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        FormsModule,
+        RouterModule,
+        ComponentsModule,
+        NgbModule,
+        NouisliderModule,
+        JwBootstrapSwitchNg2Module
+    ],
+    declarations: [ HomeComponent ],
+    exports: [ HomeComponent ],
+    providers: []
+})
+export class HomeModule { }
